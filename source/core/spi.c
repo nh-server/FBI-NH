8121bd113b0990a341c7503592ce7811fa4dc268
Steven Smith <Steveice10@gmail.com>
2016-06-03 16:18:28 -0700

    Add attribution notice.

--- source/core/spi.c
+++ source/core/spi.c
@@ -6,6 +6,28 @@
 #include "spi.h"
 #include "../ui/error.h"
 
+/*
+ * Based on information from TWLSaveTool, by TuxSH.
+ *  - https://github.com/TuxSH/TWLSaveTool/blob/master/source/SPI.cpp
+ *
+ * The original license is as follows:
+ *
+ *  Copyright (C) 2015-2016 TuxSH
+ *
+ *  TWLSaveTool is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 #define SPI_CMD_RDSR 5
 #define SPI_CMD_WREN 6
 #define SPI_CMD_RDID 0x9F
