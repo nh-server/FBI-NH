54572a02e15b4a89cdda2a2007192fb464c364ed
Steveice10 <Steveice10@gmail.com>
2017-02-22 22:35:33 -0800

    Fix servefiles batch script when in directory with spaces.

--- servefiles/drop cias or folder here.bat
+++ servefiles/drop cias or folder here.bat
@@ -2,10 +2,10 @@
 set /p DSIP="Enter the IP of your 3DS: "
 for %%a in (%*) do (
 	if "%%~xa"==".cia" (
-		python %~dp0servefiles.py %DSIP% "%%~a"
+		python "%~dp0servefiles.py" %DSIP% "%%~a"
 	)
 	if "%%~xa"=="" (
-		python %~dp0servefiles.py %DSIP% "%%~a"
+		python "%~dp0servefiles.py" %DSIP% "%%~a"
 	)
 )
 pause
