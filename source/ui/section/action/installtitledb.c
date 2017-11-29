6a51ab1f9fcd178715cdcc42b46562df5ef7893d
Steveice10 <1269164+Steveice10@users.noreply.github.com>
2017-11-29 13:07:37 -0800

    Add support for installing 3DSX apps from TitleDB.

--- source/ui/section/action/installtitledb.c
+++ source/ui/section/action/installtitledb.c
@@ -19,7 +19,7 @@ static void action_update_titledb_finished(void* data) {
 
 void action_install_titledb(linked_list* items, list_item* selected) {
     char url[64];
-    snprintf(url, INSTALL_URL_MAX, "https://3ds.titledb.com/v1/cia/%lu/download", ((titledb_info*) selected->data)->id);
+    snprintf(url, INSTALL_URL_MAX, "https://3ds.titledb.com/v1/%s/%lu/download", ((titledb_info*) selected->data)->type == TITLEDB_TYPE_CIA ? "cia" : "tdsx", ((titledb_info*) selected->data)->id);
 
     action_install_url("Install the selected title from TitleDB?", url, NULL, selected, action_update_titledb_finished, action_install_titledb_draw_top);
 }
--- source/ui/section/titledb.c
+++ source/ui/section/titledb.c
@@ -12,7 +12,12 @@
 #include "../../core/linkedlist.h"
 #include "../../core/screen.h"
 
-static list_item install = {"Install", COLOR_TEXT, action_install_titledb};
+static titledb_type section_cia_type = TITLEDB_TYPE_CIA;
+static list_item section_cia = {"CIA", COLOR_TEXT, &section_cia_type};
+static titledb_type section_3dsx_type = TITLEDB_TYPE_3DSX;
+static list_item section_3dsx = {"3DSX", COLOR_TEXT, &section_3dsx_type};
+
+static list_item action_install = {"Install", COLOR_TEXT, action_install_titledb};
 
 // TODO: Updating disabled pending TitleDB pull request.
 //static list_item update_all = {"Update All", COLOR_TEXT, action_update_titledb};
@@ -21,7 +26,7 @@ typedef struct {
     populate_titledb_data populateData;
 
     bool populated;
-} titledb_data;
+} titledb_section_data;
 
 typedef struct {
     linked_list* items;
@@ -58,7 +63,7 @@ static void titledb_action_update(ui_view* view, void* data, linked_list* items,
     }
 
     if(linked_list_size(items) == 0) {
-        linked_list_add(items, &install);
+        linked_list_add(items, &action_install);
 
         // TODO: Updating disabled pending TitleDB pull request.
         //linked_list_add(items, &update_all);
@@ -79,8 +84,8 @@ static void titledb_action_open(linked_list* items, list_item* selected) {
     list_display("TitleDB Action", "A: Select, B: Return", data, titledb_action_update, titledb_action_draw_top);
 }
 
-static void titledb_draw_top(ui_view* view, void* data, float x1, float y1, float x2, float y2, list_item* selected) {
-    titledb_data* listData = (titledb_data*) data;
+static void titledb_section_draw_top(ui_view* view, void* data, float x1, float y1, float x2, float y2, list_item* selected) {
+    titledb_section_data* listData = (titledb_section_data*) data;
 
     if(!listData->populateData.itemsListed) {
         static const char* text = "Loading title list, please wait...\nNOTE: Cancelling may take up to 15 seconds.";
@@ -94,8 +99,8 @@ static void titledb_draw_top(ui_view* view, void* data, float x1, float y1, floa
     }
 }
 
-static void titledb_update(ui_view* view, void* data, linked_list* items, list_item* selected, bool selectedTouched) {
-    titledb_data* listData = (titledb_data*) data;
+static void titledb_section_update(ui_view* view, void* data, linked_list* items, list_item* selected, bool selectedTouched) {
+    titledb_section_data* listData = (titledb_section_data*) data;
 
     if(hidKeysDown() & KEY_B) {
         if(!listData->populateData.finished) {
@@ -143,15 +148,40 @@ static void titledb_update(ui_view* view, void* data, linked_list* items, list_i
     }
 }
 
-void titledb_open() {
-    titledb_data* data = (titledb_data*) calloc(1, sizeof(titledb_data));
+void titledb_section_open(titledb_type type) {
+    titledb_section_data* data = (titledb_section_data*) calloc(1, sizeof(titledb_section_data));
     if(data == NULL) {
-        error_display(NULL, NULL, "Failed to allocate TitleDB data.");
+        error_display(NULL, NULL, "Failed to allocate TitleDB section data.");
 
         return;
     }
 
+    data->populateData.type = type;
+
     data->populateData.finished = true;
 
-    list_display("TitleDB.com", "A: Select, B: Return, X: Refresh", data, titledb_update, titledb_draw_top);
+    list_display("TitleDB.com", "A: Select, B: Return, X: Refresh", data, titledb_section_update, titledb_section_draw_top);
+}
+
+static void titledb_update(ui_view* view, void* data, linked_list* items, list_item* selected, bool selectedTouched) {
+    if(hidKeysDown() & KEY_B) {
+        ui_pop();
+        list_destroy(view);
+
+        return;
+    }
+
+    if(selected != NULL && selected->data != NULL && (selectedTouched || (hidKeysDown() & KEY_A))) {
+        titledb_section_open(*(titledb_type*) selected->data);
+        return;
+    }
+
+    if(linked_list_size(items) == 0) {
+        linked_list_add(items, &section_cia);
+        linked_list_add(items, &section_3dsx);
+    }
+}
+
+void titledb_open() {
+    list_display("TitleDB.com", "A: Select, B: Return", NULL, titledb_update, NULL);
 }
