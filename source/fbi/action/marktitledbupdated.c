87914a1127f9b04ce948e87d8f81287ef765449d
Steveice10 <1269164+Steveice10@users.noreply.github.com>
2018-05-09 23:50:46 -0700

    Add option to mark installed TitleDB entry as updated.

---
+++ source/fbi/action/marktitledbupdated.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+#include <3ds.h>
+
+#include "action.h"
+#include "../task/uitask.h"
+#include "../../core/core.h"
+
+void action_mark_titledb_updated(linked_list* items, list_item* selected, bool cia) {
+    titledb_info* info = (titledb_info*) selected->data;
+
+    if((cia && info->cia.installed) || (!cia && info->tdsx.installed)) {
+        titledb_cache_entry entry;
+        if(cia) {
+            entry.id = info->cia.id;
+            strncpy(entry.mtime, info->cia.mtime, sizeof(entry.mtime));
+            strncpy(entry.version, info->cia.version, sizeof(entry.version));
+        } else {
+            entry.id = info->tdsx.id;
+            strncpy(entry.mtime, info->tdsx.mtime, sizeof(entry.mtime));
+            strncpy(entry.version, info->tdsx.version, sizeof(entry.version));
+        }
+
+        task_populate_titledb_cache_set(info->id, cia, &entry);
+        task_populate_titledb_update_status(selected);
+    }
+}
